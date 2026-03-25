11d055b16348ad61477cd6b61a1eb0278768e5a1
Ronald Leal <ronald.leal@pragma.com.co>
2026-03-25 08:28:21 -0500

    feat: refactor y mejoras de estructura + refuerzo de .gitignore
    - actualiza .gitignore para evitar subir artefactos de build, IDE y temporales
    - reorganiza y limpia estructura de carpetas en src/
    - ajusta lógica y pruebas unitarias en flujos de usuario
    - elimina archivos innecesarios o movidos en infrastructure/out

--- src/main/java/com/pragma/usuarios/infrastructure/out/jpa/repository/IRolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.usuarios.infrastructure.out.jpa.repository;
-
-import com.pragma.usuarios.infrastructure.out.jpa.entity.RolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRolRepository extends JpaRepository<RolEntity, Long> {
-}
